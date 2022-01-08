ea39e838be99fdec157b5b9c9e77a414dabf6a85
ForestFe <g1927066@t.st-kougei.ac.jp>
2022-01-09 04:30:13 +0900

    アクセル時の魔法陣を追加

---
+++ mk4inVR/Assets/+Scripts+/Player/RotateMagiCir.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMagiCir : MonoBehaviour
+{
+    [SerializeField] private float spin_speed_A = 5.0f;
+    private Quaternion rot;
+    private Quaternion now_q;
+
+    [SerializeField] ManagerScript manager = null;
+
+    void Update()
+    {
+        if (!manager.brake_event)
+        {
+            rot = Quaternion.AngleAxis(spin_speed_A, Vector3.up/*(軸)*/);
+            now_q = this.transform.rotation;
+
+            this.transform.rotation = now_q * rot;
+        }
+    }
+}
